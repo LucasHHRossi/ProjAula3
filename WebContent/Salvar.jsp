05bd28b0bd9924834acd1f338126fbfb2f6c7b68
LUCAS ROSSI <lukinharossibr12@gmail.com>
2018-02-28 22:03:24 -0300

    Salvo

--- WebContent/Salvar.jsp
+++ WebContent/Salvar.jsp
@@ -1,7 +1,8 @@
+<%@page import="br.com.proj.dados.MotoDB"%>
 <%@page import="br.com.proj.entidade.Moto"%>
 <%@page import="com.sun.org.apache.xpath.internal.operations.Mod"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,7 +10,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-<%
+	<%
 int id = Integer.parseInt(request.getParameter("id"));
 String descricao= request.getParameter("descricao");
 int ano = Integer.parseInt(request.getParameter("ano"));
@@ -19,7 +20,13 @@ Moto moto = new Moto();
 moto.setId(id);
 moto.setDescricao(descricao);
 moto.setAno(ano);
+MotoDB db = new MotoDB();
 
+ if (db.insert(moto)){
+ System.out.println("Inserido com sucesso");}
+ else{
+ System.out.println("Erro!");
+ }
 %>
 
 </body>
--- src/br/com/proj/dados/MotoDB.java
+++ src/br/com/proj/dados/MotoDB.java
@@ -1,12 +1,19 @@
 package br.com.proj.dados;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import br.com.proj.entidade.Moto;
+import br.com.proj.util.Conexao;
 
 public class MotoDB implements IMotoDB {
-	public MotoDB() {
+	private Connection con;
+	private PreparedStatement ps;
 
+	public MotoDB() {
+		con = Conexao.getConnection();
 	}
 
 	public boolean insert(Moto moto) {
@@ -14,7 +21,33 @@ public class MotoDB implements IMotoDB {
 		System.out.println("Descricao: " + moto.getDescricao());
 		System.out.println("Ano: " + moto.getAno());
 
-		return true;
+		boolean status = false;
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("INSERT INTO MOTO");
+		sb.append("(id,descricao,ano) values (?,?,?)");
+		try {
+			ps = this.con.prepareStatement(sb.toString());
+
+			ps.setInt(1, moto.getId());
+			ps.setString(2, moto.getDescricao());
+			ps.setInt(3, moto.getAno());
+			ps.execute();
+			return true;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+
 	}
 
 	@Override
